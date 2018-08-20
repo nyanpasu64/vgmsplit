e375f1aa5231b8caa6841a330582892ac045219f
nyanpasu64 <nyanpasu64@tuta.io>
2018-08-20 15:20:58 -0700

    Attempt to statically link against actually-good libgme

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-g++ \
-    towave.cpp wave_writer.c -lgme \
-    || exit
+# g++ \
+#     towave.cpp -Wl,-Bstatic wave_writer.c -L/home/nyanpasu64/src/game-music-emu/build/gme/ -l:libgme.so \
+#     || exit
+# 
+
+g++ towave.cpp wave_writer.c \
+-L/home/nyanpasu64/src/game-music-emu/build/gme/ -Wl,-rpath . -lgme \
+|| exit
+
+ls -lhd a.out
--- towave.cpp
+++ towave.cpp
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "wave_writer.h"
-#include <gme/gme.h>
+#include "gme/gme.h"
 #include <iostream>
 #include <string>
 #include <sstream>
